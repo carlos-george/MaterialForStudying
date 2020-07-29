0e71457817aaf26d8d7bb9fd764111799ed793c3
CarlosGeorge <carlosgeorge248@gmail.com>
2020-07-29 08:34:50 -0300

    Atualizando novas funcionalidade cadastro de categoria

--- bola-flix/src/components/Menu/index.js
+++ bola-flix/src/components/Menu/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/bolaflix.png';
-import ButtonLink from './components/ButtonLink';
 import './styles.css';
 import Button from '../Button';
 
 const Menu = () => {
     return (
         <nav className="menu">
-            <a href="/">
+            <Link to="/">
                 <img className="logo" src={logo} alt="bolaFlix"/>
-            </a>
+            </Link>
 
-            <Button href="/" >
+            <Button as={Link} className="button-link" to="/cadastro/video" >
                 Novo Video
             </Button>
 
